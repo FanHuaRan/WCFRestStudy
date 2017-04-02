d547b575f85e19d55e080e9b41e99c7e64b5d23c
FanHuaRan <834171100@qq.com>
2017-04-03 00:05:11 +0800

    2017/04/01 fhr

--- Wcf_EF_Service/MusicStoreRestByWebHost/Services/WCFAlbumService.svc.cs
+++ Wcf_EF_Service/MusicStoreRestByWebHost/Services/WCFAlbumService.svc.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MusicStoreRestByWebHost.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -26,5 +27,54 @@ namespace MusicStoreRestByWebHost.Services
         }
 
         // 在此处添加更多操作并使用 [OperationContract] 标记它们
+    [OperationContract]
+    [WebInvoke(Method = "GET", UriTemplate = "Albums", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+	public List<Album> findAll(){
+		return albumService.findAlbums();
+	}
+    [OperationContract]
+   [WebInvoke(Method = "GET", UriTemplate = "Album/id", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+	public Album findOne(Int32? id) {
+		return albumService.findAlbumById(id);
+	}
+    [OperationContract]
+    [WebInvoke(Method = "POST", UriTemplate = "Album", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+	public Album create(Album album) {
+		return albumService.createAlbum(album);
+	}
+	[OperationContract]
+    [WebInvoke(Method = "POST", UriTemplate = "Album", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.WrappedRequest)]
+	public Album update(Integer id,Album album) {
+		Album originAlbum=null;
+		if(id==null||(originAlbum=albumService.findAlbumById(id))==null){
+			throw new AlbumNotFoundException();
+		}
+		return albumService.editAlbum(album);
+	}
+
+	[OperationContract]
+    [WebInvoke(Method = "POST", UriTemplate = "Album", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.WrappedRequest)]
+	public void delete(@PathVariable("id") Integer id) {
+		albumService.deleteAlbum(id);
+	}
+	
+	[OperationContract]
+    [WebInvoke(Method = "GET", UriTemplate = "Album", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+	public List<Album> search(@RequestParam(value="genreId",required=false)Integer genreId,
+							  @RequestParam(value="title",required=false)String title,
+							  @RequestParam(value="minPrice",required=false,defaultValue="0")Double minPrice,
+							  @RequestParam(value="maxPrice",required=false,defaultValue="100000")Double maxPrice) {
+		String hqlString="from Album album ";
+		List<String> whereHQLS=new ArrayList<String>();
+		if(genreId!=null){
+			whereHQLS.add(String.format("album.genreId = %d",genreId));
+		}
+		if(title!=null&&!title.equals("")){
+			whereHQLS.add(String.format("album.title = '%s'",title));
+		}
+		whereHQLS.add(String.format(" album.price between %f and %f ",minPrice,maxPrice));
+		hqlString+=whereHQLS.stream().collect(Collectors.joining(" and ", "where ", " "));
+		return albumService.findAlbums(hqlString);
+	}
     }
 }
